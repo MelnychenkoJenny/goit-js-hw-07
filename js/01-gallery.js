dc71dcbc4216590192b3f5399c02b74cd5c2afef
MelnychenkoJenny <melnichenkojenny@gmail.com>
2023-03-01 15:52:45 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,83 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const containerGallery = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+      loading="lazy"
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        width="350"
+        height="240"
+      />
+    </a>
+  </div>`,
+    )
+    .join('');
+}
+
+containerGallery.insertAdjacentHTML(
+  'beforeend',
+  createGalleryMarkup(galleryItems),
+);
+
+containerGallery.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const urlOriginalImage = event.target.dataset.source;
+  createModalWindow(urlOriginalImage);
+}
+
+function createModalWindow(url) {
+  window.addEventListener('keydown', onEscKeyDown);
+  const modalWindow = basicLightbox.create(
+    `<img src="${url}" width="1280" height="900">`,
+  );
+  modalWindow.show();
+
+  function onEscKeyDown(event) {
+    if (event.code === 'Escape') {
+      modalWindow.close();
+    }
+    window.removeEventListener('keydown', onEscKeyDown);
+  }
+}
+
+// Варіант, щоб впевнитись, що  lazyload працює коректно
+//function createGalleryMarkup(items) {
+// return items
+//     .map(
+//       ({ preview, original, description }) =>
+//         `<div class="gallery__item">
+//     <a class="gallery__link" href="${original}">
+//       <img
+//       loading="lazy"
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//         width="350"
+//         height="240"
+//       />
+//     </a>
+//   </div>
+//   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nam ipsum
+//     commodi perferendis officiis eos qui saepe iste quaerat. Dignissimos minus
+//     cum consequatur reiciendis earum cupiditate amet aliquam eum at similique
+//     illum nisi voluptatum blanditiis, modi repellendus magnam ducimus! Placeat
+//     illo, nostrum quod iure exercitationem corporis molestias facere voluptatem
+//     beatae ab vitae</p>`,
+//     )
+//     .join('');
+// }
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,9 +1,9 @@
 export const galleryItems = [
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
     description: 'Hokkaido Flower',
   },
   {
@@ -60,6 +60,69 @@ export const galleryItems = [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2023/02/25/13/18/wolves-7813244_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2023/02/25/13/18/wolves-7813244_960_720.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2023/02/26/11/28/flowers-7815462_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2023/02/26/11/28/flowers-7815462_960_720.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2023/02/26/02/59/alstroemeria-7814683_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2023/02/26/02/59/alstroemeria-7814683_960_720.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2023/02/25/13/15/bird-7813220_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2023/02/25/13/15/bird-7813220_960_720.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2023/02/27/15/06/tulips-7818680_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2023/02/27/15/06/tulips-7818680_960_720.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2023/02/24/21/11/pancake-7811889_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2023/02/24/21/11/pancake-7811889_960_720.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2023/02/27/03/41/train-7817318_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2023/02/27/03/41/train-7817318_960_720.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2023/02/23/15/51/tree-bark-7809164_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2023/02/23/15/51/tree-bark-7809164_960_720.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2023/01/14/19/50/ai-generated-7718952_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2023/01/14/19/50/ai-generated-7718952_960_720.jpg',
     description: 'Lighthouse Coast Sea',
   },
 ];
